ad2af931eaf9f36e9e7307b09b4166100fef888a
Andrii Olkhovych <the.def4alt@gmail.com>
2024-02-08 18:07:12 +0100

    Add Spring Framework

---
+++ src/main/java/com/zylope/zylope/ZylopeApplication.java
@@ -0,0 +1,13 @@
+package com.zylope.zylope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZylopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZylopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zylope/zylope/ZylopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zylope.zylope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZylopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
